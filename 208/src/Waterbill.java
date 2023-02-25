5141e3f4df7b4defdf867ed10a0d5da38fa747e9
admin <admin@Bcalab4-m13>
2023-02-25 08:44:20 +0530

    1st commit done

---
+++ 208/src/Waterbill.java
@@ -0,0 +1,20 @@
+
+public class Waterbill {
+	String acct_num;
+	int num_units;
+	double unit_cost;
+	double bill_amt;
+	
+	Waterbill(String acct_num,int num_units,double unit_cost)
+	{
+		this.acct_num=acct_num;
+		this.num_units=num_units;
+		this.unit_cost=unit_cost;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
